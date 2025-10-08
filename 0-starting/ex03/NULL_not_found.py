c8aff3ee6625afd9cc34f4e7d80e77f416161b53
Pittinan Saeyang <pittinansaeyang@gmail.com>
2025-10-09 03:12:20 +0700

    ex03

---
+++ 0-starting/ex03/NULL_not_found.py
@@ -0,0 +1,23 @@
+def NULL_not_found(object: any) -> int:
+    if object is None:
+        print(f"Nothing: {object} {type(object)}")
+        return 0
+    elif isinstance(object, float) and str(object) == "nan":
+        print(f"Cheese: {object} {type(object)}")
+        return 0
+    elif object == 0 and type(object) is int:
+        print(f"Zero: {object} {type(object)}")
+        return 0
+    elif object == "" and type(object) is str:
+        print(f"Empty: {type(object)}")
+        return 0
+    elif object is False and type(object) is bool:
+        print(f"Fake: {object} {type(object)}")
+        return 0
+    else:
+        print("Type not Found")
+        return 1
+
+
+if __name__ == "__main__":
+    pass
