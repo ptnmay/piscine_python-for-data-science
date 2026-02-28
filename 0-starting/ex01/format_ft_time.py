c3a491225bc9f27541c6d68a007e18275ada3d79
Pittinan Saeyang <pittinansaeyang@gmail.com>
2026-03-01 00:17:53 +0700

    ex01

--- 0-starting/ex01/format_ft_time.py
+++ 0-starting/ex01/format_ft_time.py
@@ -5,7 +5,8 @@ from datetime import datetime
 timestamp = time.time()
 
 # Print seconds since Jan 1, 1970 in both normal and scientific format
-print(f"Seconds since January 1, 1970: {timestamp:,.4f} or {timestamp:.2e} in scientific notation")
+print(f"Seconds since January 1, 1970: {timestamp:,.4f} or \
+      {timestamp:.2e} in scientific notation")
 
 # Get the current date and format as "Mon DD YYYY"
 current_date = datetime.now().strftime("%b %d %Y")
