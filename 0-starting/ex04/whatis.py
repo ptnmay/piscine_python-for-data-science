dbf4cad1ccb79aff762edd741c987897fdff7e7b
Pittinan Saeyang <pittinansaeyang@gmail.com>
2026-03-01 00:19:01 +0700

    ex04

--- 0-starting/ex04/whatis.py
+++ 0-starting/ex04/whatis.py
@@ -1,19 +1,20 @@
 import sys
 
 try:
-    ac = len(sys.argv)
-    if ac == 1:
+    if len(sys.argv) == 1:
         sys.exit()
 
-    assert ac <= 2, "more than one argument is provided"
-    av = sys.argv[1]
-    assert av.lstrip("-").isdigit(), "argument is not an intiger"
-    num = int(av)
+    assert len(sys.argv) == 2, "more than one argument is provided"
+
+    try:
+        num = int(sys.argv[1])
+    except ValueError:
+        raise AssertionError("argument is not an integer")
 
     if num % 2 == 0:
-        print("I'm Even")
+        print("I'm Even.")
     else:
-        print("I'm Odd")
+        print("I'm Odd.")
 
 except AssertionError as error:
     print(f"AssertionError: {error}")
